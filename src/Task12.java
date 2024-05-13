c269c91b55afd8bf0f9d3c4164f97e8e4eed8893
ElenaAndreeva9 <elenaolen92@mail.ru>
2024-05-13 22:48:46 +0400

    add Task_12

---
+++ src/Task12.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.println("Задание 12");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("ВВедите шестизначное число: ");
+        int n = scanner.nextInt();
+
+        int left = (n/ 100000)% 10 +(n/10000)% 10 +(n/1000)% 10;
+        int right = (n/ 100)% 10 +(n/10)% 10 +n% 10;
+        if(left ==  right) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
